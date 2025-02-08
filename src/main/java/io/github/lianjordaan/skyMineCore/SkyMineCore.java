a5e9920dc00ce7f0b6343e6e09cbfd347d459c29
LianJordaan <lianjordaan30052005@gmail.com>
2025-02-08 07:25:54 +0200

    First commit

---
+++ src/main/java/io/github/lianjordaan/skyMineCore/SkyMineCore.java
@@ -0,0 +1,17 @@
+package io.github.lianjordaan.skyMineCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyMineCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
